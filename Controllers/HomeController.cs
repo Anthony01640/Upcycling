0295a62e4cfe65cf599a09d01b70497c3a2ff8ea
Arnaud PIZZETTA <arnaudpizzetta@live.com>
2020-05-04 14:35:21 +0200

    Adaptation du menu pour le front

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,5 +39,9 @@ namespace UpCycling.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult AboutTest()
+        {
+            return View();
+        }
     }
 }
